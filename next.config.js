2dde06cb46337805faee98cfb34d5798df8050d7
Kevin Luu <kluu@epictech.cm>
2020-03-20 15:53:11 -0400

    add screenshot

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_URI: "mongodb+srv://user:user@cluster0-xyngf.mongodb.net/test?retryWrites=true&w=majority",
-    API_KEY: "8f0ec91b292a52af917997558257a11a"
+    MONGO_URI: <YOUR MONGODB CONNECTION STRING GOES HERE>,
+    API_KEY: <YOUR NOMICS API KEY GOES HERE>
   }
 };
